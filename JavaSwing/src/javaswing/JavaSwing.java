8eefbb33ccba3357fac77cc07045ea9522ce35da
hola@jorgemht.dev <hola@jorgemht.dev>
2015-06-14 15:11:03 +0200

    Se ha creado algunos componentes en el proyecto JavaSwing

--- JavaSwing/src/javaswing/JavaSwing.java
+++ JavaSwing/src/javaswing/JavaSwing.java
@@ -15,7 +15,10 @@ public class JavaSwing {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        Principal principal = new Principal();
+        principal.setVisible(true);
+        
     }
     
 }
